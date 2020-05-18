f672196f13d77dc2b00c9906f03591abd9140745
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-05-17 19:33:42 -0700

    pre-lecture setup

--- moai-axe-tree/src/App.js
+++ moai-axe-tree/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header';
+import Game from './components/Game';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <Game />
     </div>
   );
 };
